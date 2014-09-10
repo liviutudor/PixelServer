53fdf10831e8a013fcb103be3d123ff8e6c33edd
liviutudor <me@liviutudor.com>
2014-09-10 16:18:47 -0700

    fix imports

--- src/main/java/liv/HomeController.java
+++ src/main/java/liv/HomeController.java
@@ -1,9 +1,7 @@
 package liv;
 
 import java.io.IOException;
-import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
